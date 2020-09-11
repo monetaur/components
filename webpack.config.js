48a913424347fc1785d55660e4e362175747d1a5
Jeffrey Smith <jsmith@gasbuddy.com>
2020-09-10 22:09:50 -0400

    chore: Add webpack-node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   entry: './src/index.js',
+  externals: [
+    nodeExternals(),
+  ],
   output: {
     filename: 'index.js',
   },
