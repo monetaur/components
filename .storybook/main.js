c53c0f1146ee0ab7b9170acecf08fb7012663641
Jeffrey Smith <jsmith@gasbuddy.com>
2020-09-30 16:47:40 -0400

    chore: Upgrade Storybook to v6

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    '../src/**/*.stories.@(js|jsx)',
+  ],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+  ],
+};
